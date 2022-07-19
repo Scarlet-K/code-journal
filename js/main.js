57c5e082eaf53154661a7bfb2b5afa814a5cda7c
Scarlet Kim <scarletkim714@gmail.com>
2022-07-19 00:22:45 +0000

    Listen for 'submit' events on journal entry form and: put form's input values into a new object + add the nextEntryId to the object + increment the nextEntryId on the data model + prepend the new object to the entries in the data model + reset the image preview's src attribute + reset form inputs. #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 var $photoInput = document.querySelector('#photoURL');
 var $img = document.querySelector('img');
+var $form = document.querySelector('form');
 
 $photoInput.addEventListener('input', handlePhotoInput);
+$form.addEventListener('submit', handleSubmit);
 
 function handlePhotoInput(event) {
   $img.setAttribute('src', $photoInput.value);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  var newEntry = {};
+  newEntry.title = $form.title.value;
+  newEntry.photoURL = $form.title.value;
+  newEntry.notes = $form.notes.value;
+  newEntry.entryId = data.nextEntryId;
+  data.nextEntryId++;
+  data.entries.unshift(newEntry);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 }
